2763595501b648be52fb70fd68d53ac70e7cc97d
2dam <2dam@HZ341352.TartangaLH.eus>
2019-10-04 11:57:48 +0200

    Commit aqui tengo la funcionalidad de los botones

--- src/ejerciciodin2/Application.java
+++ src/ejerciciodin2/Application.java
@@ -5,7 +5,7 @@
  */
 package ejerciciodin2;
 
-import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,11 +15,11 @@ import javafx.stage.Stage;
  *
  * @author 2dam
  */
-public class Application extends Application {
+public class Application extends javafx.application.Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ui/FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
--- src/ejerciciodin2/ui/FXMLDocumentController.java
+++ src/ejerciciodin2/ui/FXMLDocumentController.java
@@ -1,35 +1,98 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package ejerciciodin2.ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 
 /**
  *
  * @author 2dam
  */
-public class FXMLDocumentController implements Initializable {
+public class FXMLDocumentController {
     
     @FXML
-    private Label label;
+    private Label lbl_nCuenta;
+    @FXML
+    private Button btn_mostrar;
+    @FXML
+    private TableView table;
+    @FXML
+    private TextField txtf_nCuenta;
+    @FXML
+    private Label lbl_mError;
+    @FXML
+    private Button btn_volver;
     
+    @FXML
+    private void mostrarAction(){
+        
+    }
+    /*
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    
+    }
+    */
+    /**
+     * Este metodo se ejecuta solo si es publico y sin parametros llamada initialize
+     */
+    @FXML
+    public void initialize() {
+        // TODO
+        
+        //Label de error no contendrá texto.
+        lbl_mError.setText(" ");
+        //Campo nº cuenta habilitado.
+        txtf_nCuenta.setDisable(false);
+        //Botón mostrar habilitado.
+        btn_mostrar.setDisable(false);
+        //Tabla movimientos vacio
+        //Botón Volver habilitado
+        btn_volver.setDisable(false);
+        //Foco campo nºcuenta
+        txtf_nCuenta.requestFocus();
+        
+    }
+    @FXML
+    public void onActionMostrarBuscaCuenta(){
+        if(esNumerico(txtf_nCuenta.getText())){
+            if(txtf_nCuenta.getText().length()==10){
+                //BUSCA
+            }else{
+                lbl_mError.setText("El numero de cuenta tiene que contener 10 digitos");
+            }
+        }else{
+            lbl_mError.setText("El numero de cuenta solo contiene digitos");
+        }
+        
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    @FXML
+    public void btnVolver(){
+        Platform.exit();
+    }
     
+    private boolean esNumerico(String frase){
+        boolean ret=true;
+        for(int i=0;i<frase.length();i++){
+            if(Character.isAlphabetic(frase.charAt(i))){
+                ret=false;
+                break;
+            }
+        }
+        return ret;
+    }
 }

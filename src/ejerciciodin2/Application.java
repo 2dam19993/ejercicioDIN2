a0b5f90439a1bc6110008469e097daef4ce37340
2dam <2dam@MACINF02.TartangaLH.eus>
2019-10-04 11:31:33 +0200

    2do comit

--- src/ejerciciodin2/Application.java
+++ src/ejerciciodin2/Application.java
@@ -5,31 +5,31 @@
  */
 package ejerciciodin2;
 
-import javafx.application.Application;
+
+import ejerciciodin2.ui.FXMLDocumentController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 /**
  *
  * @author 2dam
  */
-public class Application extends Application {
+public class Application extends javafx.application.Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        FXMLLoader loader= new FXMLLoader(Application.class.getResource("ui/FXMLDocument.fxml"));
+        Parent root = (Parent)loader.load();
         
-        Scene scene = new Scene(root);
-        
+       
+         Scene scene=new Scene(root);
+        //Set scene in stage and show it.
         stage.setScene(scene);
-        stage.show();
+        stage.show();      
     }
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
--- src/ejerciciodin2/ui/FXMLDocumentController.java
+++ src/ejerciciodin2/ui/FXMLDocumentController.java
@@ -5,31 +5,49 @@
  */
 package ejerciciodin2.ui;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
  * @author 2dam
  */
-public class FXMLDocumentController implements Initializable {
-    
+public class FXMLDocumentController{
+    private Stage stage;
     @FXML
-    private Label label;
-    
+    private TableView table;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField txtf_nCuenta;
+    @FXML
+    private Label lbl_nCuenta;
+    @FXML
+    private Button btn_mostrar;
+    @FXML
+    private Button btn_volver;
+    @FXML
+    private Label lbl_mError;
+    @FXML
+    private void mostrar(ActionEvent event){
+        if(lbl_nCuenta.getText().length()!=0 && lbl_nCuenta.getText().length()<=20 );
+            
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML
+    private void volver(ActionEvent event) {
+        Platform.exit();
+                
+    }
+     
+    public void initialize(){
+        //Foco campo nºcuenta
+        
+ 
+        //Label de error no contendrá texto.
+        lbl_mError.setText("");
+    }   
 }
